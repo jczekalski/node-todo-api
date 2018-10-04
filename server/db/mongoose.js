87efbc7ceb618ec551fccb4de84bedc0b82e7975
John Czekalski <john.czekalski@gmail.com>
2018-10-04 19:58:22 +0200

    Heroku fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://jncz:todoapp123@ds223253.mlab.com:23253/todo-app-api'
-};
-mongoose.connect(process.env.PORT ? db.localhost : db.mlab);
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://jncz:todoapp123@ds223253.mlab.com:23253/todo-app-api'
+// };
+mongoose.connect('mongodb://jncz:todoapp123@ds223253.mlab.com:23253/todo-app-api');
 
 module.exports = {
   mongoose
