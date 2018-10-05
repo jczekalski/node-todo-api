9bd239689b6cfd8acb377a7905dc0526bf9b541e
John Czekalski <john.czekalski@gmail.com>
2018-10-05 12:35:23 +0200

    Ready to deploy

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//Production db
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+const db = {
+  local: 'mongodb://localhost:27017/TodoApp',
+  mongo: 'mongodb://jncz:todoapp123@ds223253.mlab.com:23253/todo-app-api'
+}
 
-//Deployment db
-// mongoose.connect('mongodb://jncz:todoapp123@ds223253.mlab.com:23253/todo-app-api');
+mongoose.connect(db.mongo || db.local);
 
 module.exports = {
   mongoose
