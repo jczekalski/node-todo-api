06ea0165d510105a28ae8303b88a1f9c63f9c413
John Czekalski <john.czekalski@gmail.com>
2018-10-05 15:39:00 +0200

    config.js fix

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,9 @@
-const env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'development';
 
-const db = {
-  dev: 'mongodb://localhost:27017/TodoApp',
-  test: 'mongodb://localhost:27017/TodoAppTest',
-  mlab: 'mongodb://jncz:todoapp123@ds223253.mlab.com:23253/todo-app-api'
-};
-
-if (process.env.PORT) {
-  process.env.MONGODB_URI = db.mlab;
-} else {
+if (env === 'development') {
   process.env.PORT = 3000;
-  if (env === 'development') {
-    process.env.MONGODB_URI = db.dev;
-  } else if (env === 'test') {
-    process.env.MONGODB_URI = db.test;
-  }
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
